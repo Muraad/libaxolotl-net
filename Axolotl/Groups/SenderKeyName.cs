4e520aa771fc9c995b7bc9675fe66f538420a639
DarthRamone <moshkin_alexander@outlook.com>
2015-06-01 11:24:35 +0300

    SenderKeyName, gitingone added, project cleaned from build stuff

---
+++ Axolotl/Groups/SenderKeyName.cs
@@ -0,0 +1,40 @@
+using System;
+using Axolotl;
+
+namespace Axolotl.Groups
+{
+	public class SenderKeyName
+	{
+		public string GroupID { get; private set; }
+		public AxolotlAddress Sender { get; private set; }
+
+		public SenderKeyName (string groupId, AxolotlAddress sender)
+		{
+			GroupID = groupId;
+			Sender = sender;
+		}
+
+		public string Serialize()
+		{
+			return string.Format ("{0}::{1}::{2}", GroupID, Sender.Name, Sender.DeviceID.ToString ());
+		}
+
+		public override bool Equals (object obj)
+		{
+			if (obj == null)
+				return false;
+			if (obj.GetType () != typeof(SenderKeyName))
+				return false;
+
+			SenderKeyName that = (SenderKeyName)obj;
+
+			return GroupID.Equals (that.GroupID) && Sender.Equals (that.Sender);
+		}
+
+		public override int GetHashCode ()
+		{
+			return GroupID.GetHashCode () ^ Sender.GetHashCode ();
+		}
+	}
+}
+
